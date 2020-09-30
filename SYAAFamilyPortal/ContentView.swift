92365163c2d9f0f5e87e92e48aa882283222b669
Walter Allen <wltrallen2@gmail.com>
2020-09-30 15:58:46 -0500

    Initial Commit

---
+++ SYAAFamilyPortal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SYAAFamilyPortal
+//
+//  Created by Walter Allen on 9/30/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SYAAFamilyPortal/SYAAFamilyPortalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SYAAFamilyPortalApp.swift
+//  SYAAFamilyPortal
+//
+//  Created by Walter Allen on 9/30/20.
+//
+
+import SwiftUI
+
+@main
+struct SYAAFamilyPortalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
